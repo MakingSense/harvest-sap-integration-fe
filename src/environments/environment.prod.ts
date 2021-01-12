1bacd3bcc2b55fa4e3bb05ff3ac8abcfb2b1e376
Lucas Acosta <lacosta@makingsense.com>
2021-01-12 10:46:55 -0300

    Add command to generate a production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ import { defaults } from './defaults';
 
 export const environment = {
   ...defaults,
+  apiBaseUrl: 'http://harvest-2-sap.makingsense.com/api',
+  authCallbackUrl: 'http://harvest-2-sap.makingsense.com/auth/callback',
   production: true
 };
